c95198b5f2ab6d680a36d8b765c7eda2d080ef44
meetzaveri <meethpatel96@gmail.com>
2018-06-23 17:46:06 +0530

    Refactor : Five Layer Doughnut shrimming files and made use of functional components

--- src/containers/donut.js
+++ src/containers/donut.js
@@ -1,31 +1,69 @@
-import React, { Component } from 'react';
-import FiveLayerCircle from './five-layer-donut';
-import FourLayerCircle from './four-layer-donut';
-import ThreeLayerCircle from './three-layer-donut'
-import TwoLayerCircle from './two-layer-donut';
-import ProgressBar from './progress-layer-donut';
+import React from "react";
+import FiveLayerCircle from "./five-layer-donut";
+import FourLayerCircle from "./four-layer-donut";
+import ThreeLayerCircle from "./three-layer-donut";
+import TwoLayerCircle from "./two-layer-donut";
+import ProgressBar from "./progress-layer-donut";
 
-class Donut extends Component{
-    constructor(props){
-        super(props);
-    }
-    render() {
-        const {doughnutparts} = this.props;
-        switch(doughnutparts){
-            case 5 :
-                return (<FiveLayerCircle toggleEquality={this.props.hasEqualParts} chartSize={this.props.doughnutsize} colorobject={this.props.doughnutcolors} valuesobject={this.props.doughnutvalues} />);
-            case 4 :
-                return (<FourLayerCircle toggleEquality={this.props.hasEqualParts} chartSize={this.props.doughnutsize} colorobject={this.props.doughnutcolors} valuesobject={this.props.doughnutvalues}/>);
-            case 3 :
-                return (<ThreeLayerCircle toggleEquality={this.props.hasEqualParts} chartSize={this.props.doughnutsize} colorobject={this.props.doughnutcolors} valuesobject={this.props.doughnutvalues}/>);
-            case 2 :
-                return ( <TwoLayerCircle toggleEquality={this.props.hasEqualParts} chartSize={this.props.doughnutsize} colorobject={this.props.doughnutcolors} valuesobject={this.props.doughnutvalues}/>);
-            case 1 :
-                return (<ProgressBar toggleEquality={this.props.hasEqualParts} chartSize={this.props.doughnutsize} defaultcolors={{c1:this.props.doughnutcolors,c2:'default_bg'}} progressratevalue={this.props.completionrate}/>)
-            default :
-                return (<ThreeLayerCircle toggleEquality={this.props.hasEqualParts} chartSize={this.props.doughnutsize} colorobject={{c1:'#D1A917',c2:'#2C9DC2',c3:'#D12A6A'}} valuesobject={{p1:35,p2:40,p3:25}}/>);
-        }
-    }
-}
+const Donut = props => {
+  const { doughnutparts } = props;
+
+  switch (doughnutparts) {
+    case 5:
+      return (
+        <FiveLayerCircle
+          toggleEquality={props.hasEqualParts}
+          chartSize={props.doughnutsize}
+          colorobject={props.doughnutcolors}
+          valuesobject={props.doughnutvalues}
+        />
+      );
+    case 4:
+      return (
+        <FourLayerCircle
+          toggleEquality={props.hasEqualParts}
+          chartSize={props.doughnutsize}
+          colorobject={props.doughnutcolors}
+          valuesobject={props.doughnutvalues}
+        />
+      );
+    case 3:
+      return (
+        <ThreeLayerCircle
+          toggleEquality={props.hasEqualParts}
+          chartSize={props.doughnutsize}
+          colorobject={props.doughnutcolors}
+          valuesobject={props.doughnutvalues}
+        />
+      );
+    case 2:
+      return (
+        <TwoLayerCircle
+          toggleEquality={props.hasEqualParts}
+          chartSize={props.doughnutsize}
+          colorobject={props.doughnutcolors}
+          valuesobject={props.doughnutvalues}
+        />
+      );
+    case 1:
+      return (
+        <ProgressBar
+          toggleEquality={props.hasEqualParts}
+          chartSize={props.doughnutsize}
+          defaultcolors={{ c1: props.doughnutcolors, c2: "default_bg" }}
+          progressratevalue={props.completionrate}
+        />
+      );
+    default:
+      return (
+        <ThreeLayerCircle
+          toggleEquality={props.hasEqualParts}
+          chartSize={props.doughnutsize}
+          colorobject={{ c1: "#D1A917", c2: "#2C9DC2", c3: "#D12A6A" }}
+          valuesobject={{ p1: 35, p2: 40, p3: 25 }}
+        />
+      );
+  }
+};
 
 export default Donut;
