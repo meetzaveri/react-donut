e490637192bc6736cf662036b9c7a375ae2fb74c
Meet Zaveri <meet.zaveri@Simforms-161.local>
2018-09-11 13:39:13 +0530

    Refactor : Trimmed extra prop definitions with spread operator and supplied default props where necessary

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import MyComponent from '../../src';
 const App = () => (<MyComponent
-  hasEqualParts={false}
-  doughnutsize="large"
+  shouldRemainEqual={false}
+  doughnutSize="large"
   doughnutparts={5}
-  doughnutcolors={{
+  paintShades={{
   c1: "#D1A917",
   c2: "#2C9DC2",
   c3: "#D12A6A",
   c4: "#535353",
   c5: "#AC6946"
 }}
-  doughnutvalues={{
+  pievalues={{
   p1: 5,
   p2: 20,
   p3: 25,
--- src/containers/two-layer-donut/oneunit.js
+++ src/containers/two-layer-donut/oneunit.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, {Component, Fragment} from "react";
 import PropTypes from "prop-types";
 import DoughnutWrapper from "../../components/wrapper";
 import Slice from "../../components/slice";
@@ -19,8 +19,7 @@ const SlicePieWrapperForEqaulParts = props => {
           clipnative
           widthoffset={props.widthoffset}
           piebg={props.paintShades[c_values[index]]}
-          degree="360"
-        />
+          degree="360"/>
       </Slice>
     </React.Fragment>
   ));
@@ -36,15 +35,13 @@ const SlicePieWrapperForIneqaulParts = props => {
       <Slice
         clipnative
         slicedegree={180 * (index + 1) - props.pValue[p_values[index]]}
-        z_axis={props.pValue[p_values[index]]}
-      >
+        z_axis={props.pValue[p_values[index]]}>
         <Pie
           mainbg={props.bgclr}
           clipnative
           widthoffset={props.widthoffset}
           piebg={props.paintShades[c_values[index]]}
-          degree={360 + props.pValue[p_values[index]]}
-        />
+          degree={360 + props.pValue[p_values[index]]}/>
       </Slice>
     </React.Fragment>
   ));
@@ -52,78 +49,85 @@ const SlicePieWrapperForIneqaulParts = props => {
 };
 
 const MultipleDoughnutWrapper = props => {
-  const chartSize = props.chartSize;
-  if (chartSize === "large") {
+  const doughnutSize = props.doughnutSize;
+  if (doughnutSize === "large") {
     return (
       <Fragment>
-        <DoughnutWrapper donutwidth="220" setpos="40" />
+        <DoughnutWrapper donutwidth="220" setpos="40"/>
         <DoughnutWrapper
           className="modified_large_wrapper"
           donutwidth="240"
-          setpos="40"
-        />
+          setpos="40"/>
       </Fragment>
     );
-  } else if (chartSize === "medium") {
+  } else if (doughnutSize === "medium") {
     return (
       <Fragment>
-        <DoughnutWrapper donutwidth="155" setpos="74" />
+        <DoughnutWrapper donutwidth="155" setpos="74"/>
         <DoughnutWrapper
           className="modified_medium_wrapper"
           donutwidth="175"
-          setpos="74"
-        />
+          setpos="74"/>
       </Fragment>
     );
-  } else if (chartSize === "small") {
+  } else if (doughnutSize === "small") {
     return (
       <Fragment>
-        <DoughnutWrapper donutwidth="110" setpos="95" />
+        <DoughnutWrapper donutwidth="110" setpos="95"/>
       </Fragment>
     );
   }
 };
 
 const Circle = props => {
-  const { p1, p2, p3, p4, p5 } = props.pievalues;
-  const paintShadesRemaining = { p2, p3, p4, p5 };
+  console.log('2', props)
+  const {p1, p2, p3, p4, p5} = props.pievalues;
+  const paintShadesRemaining = {
+    p2,
+    p3,
+    p4,
+    p5
+  };
   return (
     <div>
-      {props.shouldRemainEqual ? (
-        <div style={{ position: "relative" }}>
-          <Slice slicedegree="0">
-            <Pie
-              mainbg={props.bgclr}
-              widthoffset={props.widthoffset}
-              piebg={props.paintShades.c1}
-              degree="360"
-            />
-          </Slice>
-          {/* Except for first slice-pie wrapper, other are just dynamically rendered components*/}
-          <SlicePieWrapperForEqaulParts {...props} />
-        </div>
-      ) : (
-        <div className="wrapper" style={{ position: "relative" }}>
-          <Slice piebg="yellow">
-            <Slice slicedegree={0 - p1}>
+      {props.shouldRemainEqual
+        ? (
+          <div style={{
+            position: "relative"
+          }}>
+            <Slice slicedegree="0">
               <Pie
                 mainbg={props.bgclr}
                 widthoffset={props.widthoffset}
                 piebg={props.paintShades.c1}
-                degree={360 + p1}
-              />
+                degree="360"/>
             </Slice>
             {/* Except for first slice-pie wrapper, other are just dynamically rendered components*/}
-            <SlicePieWrapperForIneqaulParts
-              widthoffset={props.widthoffset}
-              pValue={paintShadesRemaining}
-              paintShades={props.paintShades}
-              bgclr={props.bgclr}
-            />
-          </Slice>
-        </div>
-      )}
-      <MultipleDoughnutWrapper chartSize={props.chartSize} />
+            <SlicePieWrapperForEqaulParts {...props}/>
+          </div>
+        )
+        : (
+          <div className="wrapper" style={{
+            position: "relative"
+          }}>
+            <Slice piebg="yellow">
+              <Slice slicedegree={0 - p1}>
+                <Pie
+                  mainbg={props.bgclr}
+                  widthoffset={props.widthoffset}
+                  piebg={props.paintShades.c1}
+                  degree={360 + p1}/>
+              </Slice>
+              {/* Except for first slice-pie wrapper, other are just dynamically rendered components*/}
+              <SlicePieWrapperForIneqaulParts
+                widthoffset={props.widthoffset}
+                pValue={paintShadesRemaining}
+                paintShades={props.paintShades}
+                bgclr={props.bgclr}/>
+            </Slice>
+          </div>
+        )}
+      <MultipleDoughnutWrapper doughnutSize={props.doughnutSize}/>
     </div>
   );
 };
@@ -133,6 +137,17 @@ Circle.propTypes = {
   pievalues: PropTypes.object,
   paintShades: PropTypes.object,
   bgclr: PropTypes.string
+};
+
+Circle.defaultProps = {
+  paintShades: {
+    c1: "#D1A917",
+    c2: "#2C9DC2"
+  },
+  pievalues: {
+    p1: 5,
+    p2: 20
+  }
 };
 
 export default Circle;
